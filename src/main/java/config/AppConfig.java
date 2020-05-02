501d06e99c940b4dd06792ef2c1e4b7240b66827
TranKDuy <62735340+TranKDuy@users.noreply.github.com>
2020-05-02 09:55:04 +0700

    Starting Project

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,4 +1,14 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+@ComponentScan
 public class AppConfig {
+
 }
